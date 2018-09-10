a70981d11b7a2695f384d67c7163b1114e5c9318
Nico Peeters <nico@itwhiz.be>
2018-09-10 16:13:18 +0200

    Fix sign transaction

--- src/views/SignTransactionView.vue
+++ src/views/SignTransactionView.vue
@@ -15,76 +15,81 @@
 </template>
 
 <script lang='ts'>
-    import {Component, Vue} from 'vue-property-decorator';
-    import Numpad from '@/components/molecules/Numpad.vue';
-    import {EVENT_TYPES} from '../types/EventTypes';
-    import {CHAIN_TYPES} from '../types/ChainTypes';
-    import Utils from '@/utils/Utils';
-    import ResponseBody from '@/api/ResponseBody';
-    import VechainTransactionData from '@/api/VechainTransactionData';
-    import EthereumTransactionData from '@/api/EthereumTransactionData';
-    import {State} from 'vuex-class';
+import {Component, Vue} from 'vue-property-decorator';
+import Numpad from '@/components/molecules/Numpad.vue';
+import {EVENT_TYPES} from '../types/EventTypes';
+import {CHAIN_TYPES} from '../types/ChainTypes';
+import Utils from '@/utils/Utils';
+import ResponseBody from '@/api/ResponseBody';
+import VechainTransactionData from '@/api/VechainTransactionData';
+import EthereumTransactionData from '@/api/EthereumTransactionData';
+import {State} from 'vuex-class';
+import Security from '../Security';
 
-    declare const window: Window;
+declare const window: Window;
 
-    @Component({
-        components: {
-            Numpad,
-        },
-    })
-    export default class SignTransactionView extends Vue {
-        public loadingText = 'Checking credentials ...';
-        public params: EthereumTransactionData | VechainTransactionData = new EthereumTransactionData();
+@Component({
+    components: {
+        Numpad,
+    },
+})
+export default class SignTransactionView extends Vue {
+    public loadingText = 'Checking credentials ...';
+    public params: EthereumTransactionData | VechainTransactionData = new EthereumTransactionData();
 
-        @State
-        public auth: any;
+    @State
+    public auth: any;
 
-        private isEventSet = false;
-        private event!: MessageEvent;
+    private isEventSet = false;
+    private event!: MessageEvent;
 
-        public created() {
-            if (this.auth) {
-                this.addEventListeners();
-            } else {
-                this.loadingText = 'Not authenticated, going back in 3 seconds.';
-                setTimeout(() => {
-                    (window as any).close();
-                }, 3000);
-            }
-        }
-
-        public sendTransactionSignedMessage(result: ResponseBody) {
-            (this.event.source as Window).postMessage({
-                type: EVENT_TYPES.TRANSACTION_SIGNED,
-                data: result,
-            }, this.event.origin);
-            this.isEventSet = false;
-        }
-
-        private addEventListeners() {
-            window.addEventListener('message', (event: MessageEvent) => {
-                if (!this.isEventSet && Utils.isWhitelistedOrigin(event.origin)) {
-                    const data = event.data;
-                    if (data && data.type === EVENT_TYPES.SEND_PARAMS) {
-                        switch (data.chain) {
-                            case CHAIN_TYPES.VECHAIN:
-                                this.params = Object.assign(new VechainTransactionData(), data.params);
-                                break;
-                            default:
-                                this.params = Object.assign(new EthereumTransactionData(), data.params);
-                        }
-                        this.event = event;
-                        this.isEventSet = true;
-                    }
-                }
-            }, false);
-            window.addEventListener('beforeunload', () => {
-                (this.event.source as Window).postMessage({
-                    type: EVENT_TYPES.POPUP_CLOSED,
-                }, this.event.origin);
-            });
+    public created() {
+        if (this.auth) {
+            this.addEventListeners();
+        } else {
+            this.loadingText = 'Not authenticated, going back in 3 seconds.';
+            setTimeout(() => {
+                (window as any).close();
+            }, 3000);
         }
     }
+
+    public sendTransactionSignedMessage(result: ResponseBody) {
+        (this.event.source as Window).postMessage({
+            type: EVENT_TYPES.TRANSACTION_SIGNED,
+            data: result,
+        }, this.event.origin);
+        this.isEventSet = false;
+    }
+
+    private isLoggedIn() {
+        return Security.isLoggedIn;
+    }
+
+    private addEventListeners() {
+        window.addEventListener('message', (event: MessageEvent) => {
+            if (!this.isEventSet && Utils.isWhitelistedOrigin(event.origin)) {
+                const data = event.data;
+                if (data && data.type === EVENT_TYPES.SEND_PARAMS) {
+                    switch (data.chain) {
+                        case CHAIN_TYPES.VECHAIN:
+                            this.params = Object.assign(new VechainTransactionData(), data.params);
+                            break;
+                        default:
+                            this.params = Object.assign(new EthereumTransactionData(), data.params);
+                    }
+                    this.event = event;
+                    this.isEventSet = true;
+                }
+            }
+        }, false);
+        window.addEventListener('beforeunload', () => {
+            (this.event.source as Window).postMessage({
+                type: EVENT_TYPES.POPUP_CLOSED,
+            }, this.event.origin);
+        });
+    }
+}
 </script>
 
 <style lang='sass' scoped>
