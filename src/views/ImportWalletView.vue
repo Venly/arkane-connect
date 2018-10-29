1e8223ae9ba278340b49c1729b2b4114a2dfab7c
Nico Peeters <nico@itwhiz.be>
2018-10-29 16:17:51 +0100

    36 - Import Ethereum (private key) and VeChain (keystore) wallets

--- src/views/ImportWalletView.vue
+++ src/views/ImportWalletView.vue
@@ -32,156 +32,159 @@
 </template>
 
 <script lang="ts">
-import {Component, Vue} from 'vue-property-decorator';
-import WalletCard from '@/components/molecules/WalletCard.vue';
-import MasterPinDialog from '@/components/organisms/dialogs/MasterPinDialog.vue';
-import SetMasterPinDialog from '@/components/organisms/dialogs/SetMasterPinDialog.vue';
-import RedirectDialog from '@/components/organisms/dialogs/RedirectDialog.vue';
-import DialogTemplate from '@/components/molecules/DialogTemplate.vue';
-import ActionButton from '@/components/atoms/ActionButton.vue';
-import {Wallet} from '../models/Wallet';
-import {Getter, State} from 'vuex-class';
-import {SecretType} from '@/models/SecretType';
-import SvgCross from '../components/atoms/SvgCross.vue';
-import ErrorDialog from '../components/organisms/dialogs/ErrorDialog.vue';
-import ImportWalletDialog from '../components/organisms/dialogs/ImportWalletDialog.vue';
-import {Chain} from '../models/Chain';
-import Api from '../api';
+    import {Component, Vue} from 'vue-property-decorator';
+    import WalletCard from '@/components/molecules/WalletCard.vue';
+    import MasterPinDialog from '@/components/organisms/dialogs/MasterPinDialog.vue';
+    import SetMasterPinDialog from '@/components/organisms/dialogs/SetMasterPinDialog.vue';
+    import RedirectDialog from '@/components/organisms/dialogs/RedirectDialog.vue';
+    import DialogTemplate from '@/components/molecules/DialogTemplate.vue';
+    import ActionButton from '@/components/atoms/ActionButton.vue';
+    import {Wallet} from '../models/Wallet';
+    import {Getter, State} from 'vuex-class';
+    import {SecretType} from '@/models/SecretType';
+    import SvgCross from '../components/atoms/SvgCross.vue';
+    import ErrorDialog from '../components/organisms/dialogs/ErrorDialog.vue';
+    import ImportWalletDialog from '../components/organisms/dialogs/ImportWalletDialog.vue';
+    import {Chain} from '../models/Chain';
+    import Api from '../api';
 
-@Component({
-    components: {
-        ImportWalletDialog,
-        ErrorDialog,
-        SvgCross,
-        ActionButton,
-        RedirectDialog,
-        MasterPinDialog,
-        SetMasterPinDialog,
-        DialogTemplate,
-        WalletCard,
-    },
-})
-export default class ImportWalletView extends Vue {
+    @Component({
+        components: {
+            ImportWalletDialog,
+            ErrorDialog,
+            SvgCross,
+            ActionButton,
+            RedirectDialog,
+            MasterPinDialog,
+            SetMasterPinDialog,
+            DialogTemplate,
+            WalletCard,
+        },
+    })
+    export default class ImportWalletView extends Vue {
 
-    @State
-    public hasMasterPin!: boolean;
-    @State
-    public chain!: Chain;
+        @State
+        public hasMasterPin!: boolean;
+        @State
+        public chain!: Chain;
 
-    @Getter
-    public secretType!: SecretType;
-    @Getter
-    public thirdPartyClientId!: string;
+        @Getter
+        public secretType!: SecretType;
+        @Getter
+        public thirdPartyClientId!: string;
 
-    private wallet!: Wallet;
+        private wallet!: Wallet;
 
-    private isMasterPinEntered = false;
-    private isWalletPresent = false;
-    private isWalletImportFailed = false;
+        private isMasterPinEntered = false;
+        private isWalletPresent = false;
+        private isWalletImportFailed = false;
 
-    private timeleft = 5000;
-    private redirectUri = '/';
-    private interval!: any;
+        private timeleft = 5000;
+        private redirectUri = '/';
+        private interval!: any;
 
-    private privateKey: string = '';
-    private keystore: string = '';
-    private keystorePassword: string = '';
+        private privateKey: string = '';
+        private keystore: string = '';
+        private keystorePassword: string = '';
 
-    public mounted(): void {
-        this.redirectUri = (this.$route.query as any).redirectUri;
-    }
+        public mounted(): void {
+            this.redirectUri = (this.$route.query as any).redirectUri;
+        }
 
-    public get chainName(): string {
-        return this.chain.name;
-    }
+        public get chainName(): string {
+            return this.chain.name;
+        }
 
-    private get showImportWallet(): boolean {
-        return this.privateKeyIsEmpty && this.keystoreIsEmpty;
-    }
+        private get showImportWallet(): boolean {
+            return this.privateKeyIsEmpty && this.keystoreIsEmpty;
+        }
 
-    private showEnterMasterPin(): boolean {
-        return !(this.privateKeyIsEmpty && this.keystoreIsEmpty) && !this.isMasterPinEntered;
-    }
+        private showEnterMasterPin(): boolean {
+            return !(this.privateKeyIsEmpty && this.keystoreIsEmpty) && !this.isMasterPinEntered;
+        }
 
-    private get showImportingWallet(): boolean {
-        return !(this.privateKeyIsEmpty && this.keystoreIsEmpty) && this.isMasterPinEntered && !this.isWalletPresent && !this.isWalletImportFailed;
-    }
+        private get showImportingWallet(): boolean {
+            return !(this.privateKeyIsEmpty && this.keystoreIsEmpty) && this.isMasterPinEntered && !this.isWalletPresent && !this.isWalletImportFailed;
+        }
 
-    private get showWalletImported(): boolean {
-        return !(this.privateKeyIsEmpty && this.keystoreIsEmpty) && this.isMasterPinEntered && this.isWalletPresent;
-    }
+        private get showWalletImported(): boolean {
+            return !(this.privateKeyIsEmpty && this.keystoreIsEmpty) && this.isMasterPinEntered && this.isWalletPresent;
+        }
 
-    private get showWalletImportFailed(): boolean {
-        return !(this.privateKeyIsEmpty && this.keystoreIsEmpty) && this.isMasterPinEntered && !this.isWalletPresent && this.isWalletImportFailed;
-    }
+        private get showWalletImportFailed(): boolean {
+            return !(this.privateKeyIsEmpty && this.keystoreIsEmpty) && this.isMasterPinEntered && !this.isWalletPresent && this.isWalletImportFailed;
+        }
 
-    private get privateKeyIsEmpty() {
-        return this.privateKey === '';
-    }
+        private get privateKeyIsEmpty() {
+            return this.privateKey === '';
+        }
 
-    private get keystoreIsEmpty() {
-        return this.keystore === '' && this.keystorePassword === '';
-    }
+        private get keystoreIsEmpty() {
+            return this.keystore === '' && this.keystorePassword === '';
+        }
 
-    private importPrivateKey(privateKey: string): void {
-        this.privateKey = privateKey;
-    }
+        private importPrivateKey(privateKey: string): void {
+            this.privateKey = privateKey;
+        }
 
-    private importKeystore(keystore: string, keystorePassword: string) {
-        this.keystore = keystore;
-        this.keystorePassword = keystorePassword;
-    }
+        private importKeystore(keystore: string, keystorePassword: string) {
+            this.keystore = keystore;
+            this.keystorePassword = keystorePassword;
+        }
 
-    private async masterpinEntered(pincode: string) {
-        if (pincode) {
-            this.isMasterPinEntered = true;
-            this.$store.dispatch('startLoading');
-            this.importWallet(pincode)
-                .then((newWallet: Wallet) => {
-                    this.wallet = newWallet;
-                    this.isWalletPresent = !!(this.wallet);
-                    this.$store.dispatch('stopLoading');
-                    this.interval = setInterval(() => {
-                        this.timeleft = this.timeleft - 1000;
-                        if (this.timeleft <= 0) {
-                            clearInterval(this.interval);
-                        }
-                    }, 1000);
-                })
-                .catch((reason: any) => {
-                    this.$store.dispatch('stopLoading');
-                    this.isWalletImportFailed = true;
-                });
+        private async masterpinEntered(pincode: string) {
+            if (pincode) {
+                this.isMasterPinEntered = true;
+                this.$store.dispatch('startLoading');
+                this.importWallet(pincode)
+                    .then((newWallet: Wallet) => {
+                        this.wallet = newWallet;
+                        this.isWalletPresent = !!(this.wallet);
+                        this.$store.dispatch('stopLoading');
+                        this.interval = setInterval(() => {
+                            this.timeleft = this.timeleft - 1000;
+                            if (this.timeleft <= 0) {
+                                clearInterval(this.interval);
+                            }
+                        }, 1000);
+                    })
+                    .catch((reason: any) => {
+                        this.$store.dispatch('stopLoading');
+                        this.isWalletImportFailed = true;
+                    });
+            }
+        }
+
+        private async importWallet(pincode: string): Promise<Wallet> {
+            if (!this.privateKeyIsEmpty) {
+                const wallet = await this.$store.dispatch('importPrivateKey', {importWalletType: this.chain.importWalletType, pincode, privateKey: this.privateKey});
+                await Api.linkWallet({client: this.thirdPartyClientId, walletIds: [wallet.id]});
+                return wallet;
+            } else if (!this.keystoreIsEmpty) {
+                const wallet = await this.$store.dispatch(
+                    'importKeystore',
+                    {importWalletType: this.chain.importWalletType, pincode, keystore: this.keystore, password: this.keystorePassword},
+                );
+                await Api.linkWallet({client: this.thirdPartyClientId, walletIds: [wallet.id]});
+                return wallet;
+            }
+            return new Promise<Wallet>((resolve, reject) => {
+                reject();
+            });
+        }
+
+        private tryAgain() {
+            this.privateKey = '';
+            this.keystore = '';
+            this.keystorePassword = '';
+            this.isWalletImportFailed = false;
+            this.isMasterPinEntered = false;
+        }
+
+        private redirectBack() {
+            window.location.href = this.redirectUri;
         }
     }
-
-    private async importWallet(pincode: string): Promise<Wallet> {
-        if (!this.privateKeyIsEmpty) {
-            const wallet = await this.$store.dispatch('importPrivateKey', {importWalletType: this.chain.importWalletType, pincode, privateKey: this.privateKey});
-            await Api.linkWallet({client: this.thirdPartyClientId, walletIds: [wallet.id]});
-            return wallet;
-        } else if (!this.keystoreIsEmpty) {
-            const wallet = await this.$store.dispatch(
-                'importKeystore',
-                {importWalletType: this.chain.importWalletType, pincode, keystore: this.keystore, password: this.keystorePassword},
-            );
-            await Api.linkWallet({client: this.thirdPartyClientId, walletIds: [wallet.id]});
-            return wallet;
-        }
-    }
-
-    private tryAgain() {
-        this.privateKey = '';
-        this.keystore = '';
-        this.keystorePassword = '';
-        this.isWalletImportFailed = false;
-        this.isMasterPinEntered = false;
-    }
-
-    private redirectBack() {
-        window.location.href = this.redirectUri;
-    }
-}
 </script>
 
 <style lang="sass" scoped>
