2b60160407e0d20af9dabfd5789c320670f38f52
Nico Peeters <nico@itwhiz.be>
2018-10-29 08:51:32 +0100

    36 - Disable 'create' when linked wallets changed

--- src/components/organisms/dialogs/LinkWalletsDialog.vue
+++ src/components/organisms/dialogs/LinkWalletsDialog.vue
@@ -1,29 +1,29 @@
 <template>
-    <dialog-template :title="'Access to your wallets'" v-if="wallets.length > 0">
-        <p class="description no-margin-bottom">Select the wallets that application <strong>{{thirdPartyClientId}}</strong> is allowed to access:</p>
-        <form class="form">
-            <div class="wallets">
-                <div class="wallet-control" v-for="wallet in wallets">
-                    <div class="control control--checkbox">
-                        <input :ref="`wallet-${wallet.id}`" :id="`wallet-${wallet.id}`" :value="wallet" v-model="selectedWallets"
-                               class="control__checkbox control__checkbox--check wallet-select" type="checkbox"/>
-                        <label class="control__label" :for="`wallet-${wallet.id}`"></label>
-                    </div>
-                    <wallet-card :wallet="wallet" :showFunds="false" @click="walletSelected(wallet)"></wallet-card>
-                </div>
-            </div>
-        </form>
-        <action-button class="link-button"
-                       :type="'brand-light'"
-                       @click="linkWallets"
-                       :disabled="selectedWallets.length <= 0"
-                       :title="selectedWallets.length <= 0 ? 'At least one wallet needs to be selected' : ''">Link Wallets
-        </action-button>
-        <div class="separator">
-            <div class="separator__label">or</div>
+  <dialog-template :title="'Access to your wallets'" v-if="wallets.length > 0">
+    <p class="description no-margin-bottom">Select the wallets that application <strong>{{thirdPartyClientId}}</strong> is allowed to access:</p>
+    <form class="form">
+      <div class="wallets">
+        <div class="wallet-control" v-for="wallet in wallets">
+          <div class="control control--checkbox">
+            <input :ref="`wallet-${wallet.id}`" :id="`wallet-${wallet.id}`" :value="wallet" v-model="selectedWallets"
+                   class="control__checkbox control__checkbox--check wallet-select" type="checkbox"/>
+            <label class="control__label" :for="`wallet-${wallet.id}`"></label>
+          </div>
+          <wallet-card :wallet="wallet" :showFunds="false" @click="walletSelected(wallet)"></wallet-card>
         </div>
-        <action-button @click="createWallet">Create a New Wallet</action-button>
-    </dialog-template>
+      </div>
+    </form>
+    <action-button class="link-button"
+                   :type="'brand-light'"
+                   @click="linkWallets"
+                   :disabled="selectedWallets.length <= 0"
+                   :title="selectedWallets.length <= 0 ? 'At least one wallet needs to be selected' : ''">Link Wallets
+    </action-button>
+    <div class="separator">
+      <div class="separator__label">or</div>
+    </div>
+    <action-button @click="createWallet" :disabled="linkedWalletsChanged" >Create a New Wallet</action-button>
+  </dialog-template>
 </template>
 
 <script lang="ts">
@@ -59,13 +59,24 @@
         private chain!: string;
 
         private selectedWallets: Wallet[] = [];
+        private originalSelectedWalletIds: string[] = [];
 
         public async mounted() {
             this.selectedWallets = await Api.getWallets({secretType: SecretTypeUtil.byChain(this.chain), clientId: this.thirdPartyClientId});
+            this.originalSelectedWalletIds = this.selectedWallets.map((wallet: Wallet) => wallet.id);
+        }
+
+        public get linkedWalletsChanged(): boolean {
+            return this.selectedWallets.length !== this.originalSelectedWalletIds.length
+                || (this.selectedWalletsIds.filter((sw) => this.originalSelectedWalletIds.indexOf(sw) === -1).length > 0);
+        }
+
+        private get selectedWalletsIds(): string[] {
+            return this.selectedWallets.map((wallet: Wallet) => wallet.id);
         }
 
         private walletSelected(selectedWallet: Wallet) {
-            if (this.selectedWallets.indexOf(selectedWallet) > -1) {
+            if (this.selectedWallets.filter((wallet: Wallet) => wallet.id === selectedWallet.id).length > 0) {
                 this.selectedWallets = this.selectedWallets.filter((wallet: Wallet) => wallet.id !== selectedWallet.id);
             } else {
                 this.selectedWallets.push(selectedWallet);
@@ -82,43 +93,43 @@
     }
 </script>
 <style lang="sass" scoped>
-    @import "../../../assets/sass/mixins-and-vars"
+  @import "../../../assets/sass/mixins-and-vars"
 
-    .container
-        min-height: 100vh
+  .container
+    min-height: 100vh
 
-    .dialog-container
-        display: flex
-        height: 100%
-        justify-content: center
-        align-items: center
+  .dialog-container
+    display: flex
+    height: 100%
+    justify-content: center
+    align-items: center
 
-    .wallets
-        margin: rem(20px 0 20px 0)
-        padding: rem(0 10px)
-        max-height: rem(300px)
-        overflow-y: auto
-        overflow-x: hidden
+  .wallets
+    margin: rem(20px 0 20px 0)
+    padding: rem(0 10px)
+    max-height: rem(300px)
+    overflow-y: auto
+    overflow-x: hidden
 
-    .wallet-control
-        display: flex
-        align-items: center
-        margin: rem(20px 0)
-        &:first-child
-            margin-top: 0
+  .wallet-control
+    display: flex
+    align-items: center
+    margin: rem(20px 0)
+    &:first-child
+      margin-top: 0
 
-    .wallet-card
-        width: calc(100% - #{rem(35px)})
-        cursor: pointer
+  .wallet-card
+    width: calc(100% - #{rem(35px)})
+    cursor: pointer
 
-    .control--checkbox
-        margin-bottom: 0
+  .control--checkbox
+    margin-bottom: 0
 
-    .link-button
-        margin-bottom: 0
-        margin-top: rem(20px)
+  .link-button
+    margin-bottom: 0
+    margin-top: rem(20px)
 
-    .separator
-        margin: rem(30px 0)
+  .separator
+    margin: rem(30px 0)
 
 </style>
