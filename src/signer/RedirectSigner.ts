d705fc18da8543b53713e81a096c0535e7e5d723
Nico Peeters <nico.peeters@arkane.network>
2019-02-27 16:46:57 +0100

    Hotfix

--- src/signer/RedirectSigner.ts
+++ src/signer/RedirectSigner.ts
@@ -10,24 +10,33 @@ export class RedirectSigner implements Signer {
         this.bearerTokenProvider = bearerTokenProvider;
     }
 
-    public executeNativeTransaction(transactionRequest: any, options?: { redirectUri?: string, correlationID?: string }): Promise<SignerResult> {
+    public executeNativeTransaction(transactionRequest: any, options: { redirectUri?: string, correlationID?: string } = {}): Promise<SignerResult> {
+        this.checkRedirectUri(options);
         return new Promise<SignerResult>((resolve, reject) => {
             Utils.http().postInForm(`${Utils.urls.connect}/transaction/execute/${transactionRequest.type.toLowerCase()}`, transactionRequest, this.bearerTokenProvider, options);
             resolve();
         });
     }
 
-    public executeTransaction(genericTransactionRequest: GenericTransactionRequest, options?: { redirectUri?: string, correlationID?: string }): Promise<SignerResult> {
+    public executeTransaction(genericTransactionRequest: GenericTransactionRequest, options: { redirectUri?: string, correlationID?: string } = {}): Promise<SignerResult> {
+        this.checkRedirectUri(options);
         return new Promise<SignerResult>((resolve, reject) => {
             Utils.http().postInForm(`${Utils.urls.connect}/transaction/execute`, genericTransactionRequest, this.bearerTokenProvider, options);
             resolve();
         });
     }
 
-    public signTransaction(signatureRequest: any, options?: { redirectUri?: string, correlationID?: string }): Promise<SignerResult> {
+    public signTransaction(signatureRequest: any, options: { redirectUri?: string, correlationID?: string } = {}): Promise<SignerResult> {
+        this.checkRedirectUri(options);
         return new Promise<SignerResult>((resolve, reject) => {
             Utils.http().postInForm(`${Utils.urls.connect}/transaction/sign/${signatureRequest.type.toLowerCase()}`, signatureRequest, this.bearerTokenProvider, options);
             resolve();
         });
+    }
+
+    private checkRedirectUri(options: { redirectUri?: string; correlationID?: string }) {
+        if (!options.redirectUri) {
+            options.redirectUri = window.location.href;
+        }
     }
 }
