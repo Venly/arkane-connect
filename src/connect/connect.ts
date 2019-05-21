a9fff5ec5f719697bf27a2f30d78da18a7df08e1
Steve Maris <steve_maris@hotmail.com>
2019-05-21 20:17:00 +0200

    ARKANE-793

--- src/connect/connect.ts
+++ src/connect/connect.ts
@@ -2,24 +2,23 @@ import { KeycloakInstance, KeycloakPromise } from 'keycloak-js';
 
 import { LoginResult, Security }             from './Security';
 import { Api }                               from '../api/Api';
-import { SecretType }                        from '../models/SecretType';
 import { Wallet }                            from '../models/wallet/Wallet';
 import { WindowMode }                        from '../models/WindowMode';
-import { GeneralPopup }                      from '../popup/GeneralPopup';
-import { PopupActions }                      from '../popup/PopupActions';
 import { PopupResult }                       from '../popup/PopupResult';
 import { PopupSigner }                       from '../signer/PopupSigner';
 import { Signer, SignerFactory, SignMethod } from '../signer/Signer';
 import Utils                                 from '../utils/Utils';
+import { Flows }                             from './Flows';
 
 export class ArkaneConnect {
 
     public api!: Api;
     public signUsing: WindowMode;
+    public flows: Flows;
     public windowMode: WindowMode;
+    public _bearerTokenProvider: () => string;
 
     private clientId: string;
-    private bearerTokenProvider: () => string;
     private auth!: KeycloakInstance;
 
     constructor(clientId: string, options?: ConstructorOptions) {
@@ -27,62 +26,16 @@ export class ArkaneConnect {
         this.signUsing = (options && options.signUsing as unknown as WindowMode) || WindowMode.POPUP;
         this.windowMode = (options && options.windowMode) || WindowMode.POPUP;
         Utils.rawEnvironment = options && options.environment || 'prod';
-        this.bearerTokenProvider = options && options.bearerTokenProvider || (() => this.auth.token && this.auth.token || '');
-        if (this.bearerTokenProvider) {
-            this.api = new Api(Utils.urls.api, this.bearerTokenProvider);
-        }
-    }
-
-    public async getUserAndWalletsFlow(chain: SecretType): Promise<UserAndWalletsResult> {
-        let loginResult = await Security.checkAuthenticated(this.clientId);
-        let wallets: Wallet[] = [];
-
-        try {
-            // Check if authenticated
-            if (!loginResult.authenticated) {
-                loginResult = await Security.login(this.clientId, {windowMode: WindowMode.POPUP, closePopup: false});
-            }
-
-            if (loginResult.authenticated) {
-                this.auth = loginResult.keycloak;
-                wallets = await this.api.getWallets({secretType: chain.toUpperCase() as SecretType});
-                if (!(wallets && wallets.length > 0)) {
-                    const popupResult = await this.manageWallets(chain,{windowMode: WindowMode.POPUP});
-                    if (popupResult && popupResult.status === 'SUCCESS') {
-                        wallets = await this.api.getWallets({secretType: chain.toUpperCase() as SecretType});
-                    }
-                }
-            } else {
-                throw Error('Something went wrong.');
-            }
-
-            if (wallets.length === 0) {
-                throw Error('Something went wrong.');
-            }
-        } catch (e) {
-            console.error(e);
-        } finally {
-            if (loginResult && loginResult.popupWindow) {
-                loginResult.popupWindow.close();
-            }
+        this._bearerTokenProvider = options && options.bearerTokenProvider || (() => this.auth.token && this.auth.token || '');
+        if (this._bearerTokenProvider) {
+            this.api = new Api(Utils.urls.api, this._bearerTokenProvider);
         }
 
-        return {
-            wallets: wallets,
-            auth: loginResult.keycloak,
-            isAuthenticated: loginResult.authenticated
-        }
+        this.flows = new Flows(this, this.clientId);
     }
 
     public async checkAuthenticated(): Promise<AuthenticationResult> {
         const loginResult = await Security.checkAuthenticated(this.clientId);
-        return this.afterAuthentication(loginResult);
-    }
-
-    public async authenticate(options?: AuthenticationOptions): Promise<AuthenticationResult> {
-        let authOptions: AuthenticationOptions = {...options};
-        authOptions.windowMode = authOptions.windowMode || this.windowMode;
-        const loginResult = await Security.login(this.clientId, authOptions);
         return this.afterAuthentication(loginResult);
     }
 
@@ -96,58 +49,31 @@ export class ArkaneConnect {
         }
     }
 
-    public manageWallets(chain: string, options?: { redirectUri?: string, correlationID?: string, windowMode?: WindowMode }): Promise<PopupResult | void> {
-        const windowMode = options && options.windowMode || this.windowMode;
-        if (windowMode === WindowMode.REDIRECT) {
-            return this.manageWalletsRedirect(chain, options);
-        } else {
-            return this.manageWalletsPopup(chain);
-        }
-    }
-
-    private manageWalletsRedirect(chain: string, options?: { redirectUri?: string, correlationID?: string }): Promise<void> {
-        Utils.http().postInForm(
-            `${Utils.urls.connect}/wallets/manage`,
-            {chain: chain.toLowerCase()},
-            this.bearerTokenProvider,
-            options
-        );
-        return Promise.resolve();
-    }
-
-    private manageWalletsPopup(chain: string): Promise<PopupResult> {
-        return GeneralPopup.openNewPopup(PopupActions.MANAGE_WALLETS, this.bearerTokenProvider, {chain: chain.toLowerCase()});
-    }
-
-    public linkWallets(options?: { redirectUri?: string, correlationID?: string, windowMode?: WindowMode }): Promise<PopupResult | void> {
-        const windowMode = options && options.windowMode || this.windowMode;
-        if (windowMode === WindowMode.REDIRECT) {
-            return this.linkWalletsRedirect(options);
-        } else {
-            return this.linkWalletsPopup();
-        }
-    }
-
-    private linkWalletsRedirect(options?: { redirectUri?: string, correlationID?: string }): Promise<void> {
-        Utils.http().postInForm(
-            `${Utils.urls.connect}/wallets/link`,
-            {},
-            this.bearerTokenProvider,
-            options
-        );
-        return Promise.resolve();
-    }
-
-    private linkWalletsPopup(): Promise<PopupResult> {
-        return GeneralPopup.openNewPopup(PopupActions.LINK_WALLET, this.bearerTokenProvider);
-    }
-
     public createSigner(windowMode?: WindowMode): Signer {
-        return SignerFactory.createSignerFor(windowMode || this.signUsing || this.windowMode, this.bearerTokenProvider);
+        return SignerFactory.createSignerFor(windowMode || this.signUsing || this.windowMode, this._bearerTokenProvider);
     }
 
     public isPopupSigner(signer: Signer): signer is PopupSigner {
         return (<PopupSigner>signer).closePopup !== undefined;
+    }
+
+    /* Deprecated - Use flows.authenticate instead */
+    public async authenticate(options?: AuthenticationOptions): Promise<AuthenticationResult> {
+        return this.flows.authenticate(options);
+    }
+
+    /* Deprecated - Use flows.manageWallets instead */
+    public manageWallets(chain: string, options?: { redirectUri?: string, correlationID?: string, windowMode?: WindowMode }): Promise<PopupResult | void> {
+        return this.flows.manageWallets(chain, options);
+    }
+
+    /* Deprecated - Use flows.linkWallets instead */
+    public linkWallets(options?: { redirectUri?: string, correlationID?: string, windowMode?: WindowMode }): Promise<PopupResult | void> {
+        return this.flows.linkWallets(options);
+    }
+
+    public _afterAuthenticationForFlowUse(loginResult: LoginResult): AuthenticationResult {
+        return this.afterAuthentication(loginResult);
     }
 
     private afterAuthentication(loginResult: LoginResult): AuthenticationResult {
@@ -172,7 +98,7 @@ export class ArkaneConnect {
     }
 }
 
-export interface UserAndWalletsResult {
+export interface Account {
     wallets: Wallet[],
     auth: KeycloakInstance,
     isAuthenticated: boolean
