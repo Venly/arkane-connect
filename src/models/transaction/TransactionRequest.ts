d7161e3cedc96725de226eb2ebd186183b399f70
Nico Peeters <nico.peeters@arkane.network>
2019-06-05 15:01:19 +0200

    ARKANE-823 - Add 'network' to transaction and signature requests

--- src/models/transaction/TransactionRequest.ts
+++ src/models/transaction/TransactionRequest.ts
@@ -1,6 +1,9 @@
+import { Network } from '../Network';
+
 export abstract class TransactionRequest {
     public id?: string;
     public type!: string;
     public walletId!: string;
     public pincode!: string;
+    public network?: Network;
 }
