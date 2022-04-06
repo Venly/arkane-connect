2649852d418c4d9f6f6dc0880c99e6a8f627413d
ZhamakochyanErik <74008801+ZhamakochyanErik@users.noreply.github.com>
2022-04-06 15:28:05 +0400

    bug(VENLY-2922) - add txInfo mapping

---
+++ src/models/transaction/status/HederaTxInfo.ts
@@ -0,0 +1,29 @@
+import { RestApiResponseTxStatus } from './RestApiResponseTxStatus';
+
+export interface HederaTxInfo extends RestApiResponseTxStatus{
+    chargedTxFee: string;
+    consensusTimestamp: string;
+    entityId: string;
+    maxFee: string;
+    memoBase64: string;
+    name: string;
+    node: string;
+    scheduled: boolean;
+    transactionId: string;
+    validDurationSeconds: string;
+    validStartTimestamp: string;
+    result: string;
+    transfers: [HederaTransfer];
+    tokenTransfers: [HederaTokenTransfer];
+}
+
+export interface HederaTransfer{
+    account: string;
+    amount: number;
+}
+
+export interface HederaTokenTransfer{
+    tokenId: string;
+    account: string;
+    amount: number;
+}
