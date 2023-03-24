51019955054b3bc8ad156e27e49993abca6bd5ac
NicoPeeters <nico.peeters@venly.io>
2023-03-24 18:12:48 +0100

    story(VENLY-5129) - Undo dev webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,12 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
     entry: path.join(__dirname, 'src/index.ts'),
     output: {
         filename: 'connect.js',
         path: path.join(__dirname, './dist'),
     },
-    watch: true,
+    watch: false,
     module: {
         rules: [
             {
@@ -22,8 +21,5 @@ module.exports = {
     },
     resolve: {
         extensions: ['.tsx', '.ts', '.js']
-    },
-    optimization: {
-        minimize: false
     },
 };
