a18affa7430e6220f35066d94bc181d389be3dc1
David Fu <davidzwfu@gmail.com>
2023-04-25 14:40:38 +0400

    add all exports

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,78 @@
 import { VenlyConnect } from './connect/connect';
 
-export { VenlyConnect, AuthenticationOptions, AuthenticationResult }      from './connect/connect';
-export { Signer, SignMethod, SignerResult }                               from './signer/Signer';
-export { PopupSigner }                                                    from './signer/PopupSigner';
-export { RedirectSigner, RedirectOptions }                                from './signer/RedirectSigner';
-export { Api }                                                            from './api/Api';
-export { PopupResult }                                                    from './popup/PopupResult';
-export { Account }                                                        from './models/Account';
-export { SecretType }                                                     from './models/SecretType';
-export { WindowMode }                                                     from './models/WindowMode';
-export { SignatureRequestType }                                           from './models/SignatureRequestType';
-export { TransactionRequestType }                                         from './models/TransactionRequestType';
-export { Wallet }                                                         from './models/wallet/Wallet';
-export { Profile }                                                        from './models/profile/Profile';
-export {BuildTransactionRequest}                                          from './models/transaction/build/BuildTransactionRequest';
-export {TransactionRequest}                                               from './models/transaction/TransactionRequest';
-export {EthereumErc20TransactionRequest}                                  from './models/transaction/ethereum/request/EthereumErc20TransactionRequest';
-export {EthereumTransactionRequest}                                       from './models/transaction/ethereum/request/EthereumTransactionRequest';
-export {EthereumContractCallExecutionRequest}                             from './models/transaction/ethereum/request/EthereumContractCallExecutionRequest';
-export {GochainErc20TransactionRequest}                                   from './models/transaction/gochain/request/GochainErc20TransactionRequest';
-export {GochainTransactionRequest}                                        from './models/transaction/gochain/request/GochainTransactionRequest';
-export {TronTransactionRequest}                                           from './models/transaction/tron/request/TronTransactionRequest';
-export {TronContractCallExecutionRequest}                                 from './models/transaction/tron/request/TronContractCallExecutionRequest';
-export {VechainTransactionRequest, VechainTransactionRequestClause}       from './models/transaction/vechain/request/VechainTransactionRequest';
-export {VechainVip180TransactionRequest, VechainVip180TransactionClause}  from './models/transaction/vechain/request/VechainVip180TransactionRequest';
-export {VechainContractCallExecutionRequest}                              from './models/transaction/vechain/request/VechainContractCallExecutionRequest';
-export {VechainExecutableContractCall, VechainContractCallInput}          from './models/transaction/vechain/request/VechainExecutableContractCall';
-export {BitcoinTransactionRequest}                                        from './models/transaction/bitcoin/request/BitcoinTransactionRequest';
-export {LitecoinTransactionRequest}                                       from './models/transaction/litecoin/request/LitecoinTransactionRequest';
-export {WalletBalance}                                                    from './models/wallet/WalletBalance';
-export {TokenBalance}                                                     from './models/wallet/TokenBalance';
-export {TransactionResult}                                                from './models/transaction/TransactionResult';
+export * from './api/Api';
+export * from './api/ResponseBody';
+
+export * from './connect/connect';
+
+export * from './models/Account';
+export * from './models/ConfirmationRequest';
+export * from './models/ConfirmationRequestType';
+export * from './models/Network';
+export * from './models/RequestDataType';
+export * from './models/SecretType';
+export * from './models/SignatureRequestType';
+export * from './models/TransactionRequestType';
+export * from './models/WindowMode';
+
+export * from './models/contract/ContractReadRequest';
+export * from './models/contract/ContractReadRequestInputParam';
+export * from './models/contract/ContractReadRequestOutputParam';
+export * from './models/contract/ContractReadResult';
+
+export * from './models/profile/Profile';
+
+export * from './models/transaction/GenericSignatureRequest';
+export * from './models/transaction/TransactionRequest';
+export * from './models/transaction/TransactionResult';
+export * from './models/transaction/TxStatus';
+
+export * from './models/transaction/build/BuildTransactionRequest';
+export * from './models/transaction/build/BuildContractExecutionRequestDto';
+
+export * from './models/transaction/ethereum/request/EthereumContractCallExecutionRequest';
+export * from './models/transaction/ethereum/request/EthereumErc20TransactionRequest';
+export * from './models/transaction/ethereum/request/EthereumErc721TransactionRequest';
+export * from './models/transaction/ethereum/request/EthereumErcTokenTransactionRequest';
+export * from './models/transaction/ethereum/request/EthereumTransactionRequest';
+
+export * from './models/transaction/gochain/request/GochainErc20TransactionRequest';
+export * from './models/transaction/gochain/request/GochainTransactionRequest';
+
+export * from './models/transaction/matic/request/MaticContractCallExecutionRequest';
+export * from './models/transaction/matic/request/MaticErc20TransactionRequest';
+export * from './models/transaction/matic/request/MaticErc721TransactionRequest';
+export * from './models/transaction/matic/request/MaticErcTokenTransactionRequest';
+export * from './models/transaction/matic/request/MaticTransactionRequest';
+
+export * from './models/transaction/tron/request/TronContractCallExecutionRequest';
+export * from './models/transaction/tron/request/TronTransactionRequest';
+
+export * from './models/transaction/vechain/request/VechainContractCallExecutionRequest';
+export * from './models/transaction/vechain/request/VechainExecutableContractCall';
+export * from './models/transaction/vechain/request/VechainTransactionRequest';
+export * from './models/transaction/vechain/request/VechainVip180TransactionRequest';
+
+export * from './models/transaction/bitcoin/request/BitcoinTransactionRequest';
+export * from './models/transaction/litecoin/request/LitecoinTransactionRequest';
+
+export * from './models/wallet/CreateApplicationWalletRequest';
+export * from './models/wallet/CreateFirstWalletRequest';
+export * from './models/wallet/CreateWalletRequest';
+export * from './models/wallet/ImportWalletRequest';
+export * from './models/wallet/NFT';
+export * from './models/wallet/TokenBalance';
+export * from './models/wallet/Wallet';
+export * from './models/wallet/WalletBalance';
+
+export * from './popup/PopupActions';
+export * from './popup/PopupResult';
+
+export * from './signer/PopupSigner';
+export * from './signer/RedirectSigner';
+export * from './signer/Signer';
+
+export * from './types/EventTypes';
 
 if (typeof window !== 'undefined') {
     (window as any).VenlyConnect = VenlyConnect;
