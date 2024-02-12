d9e1caf663d25808061d70fcc8f2f80503361a1e
David Fu <davidzwfu@gmail.com>
2024-02-12 17:47:28 +0400

    close manageWallets popup

--- src/connect/Flows.ts
+++ src/connect/Flows.ts
@@ -96,7 +96,7 @@ export class Flows {
             if (result.isAuthenticated) {
                 wallets = await this.connect.api.getWallets({secretType: chain.toUpperCase() as SecretType});
                 if (!(wallets && wallets.length > 0)) {
-                    const popupResult = await this.manageWallets(chain, {windowMode: WindowMode.POPUP, closePopup: false});
+                    const popupResult = await this.manageWallets(chain, {windowMode: WindowMode.POPUP});
                     if (popupResult && popupResult.status === 'SUCCESS') {
                         wallets = await this.connect.api.getWallets({secretType: chain.toUpperCase() as SecretType});
                     }
@@ -109,7 +109,7 @@ export class Flows {
             console.error(e);
         }
 
-        if (Security.hasPopupWindow() && !(options && options.closePopup == false)) {
+        if (Security.hasPopupWindow()) {
             Security.closePopupWindow();
         }
 
